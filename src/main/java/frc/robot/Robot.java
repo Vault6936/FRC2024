92af9ba62e74a2a762900beeab27ffe4f2d7c039
Jetfire <app.iwhite@outlook.com>
2024-03-12 20:55:24 -0400

    Added button command for VerticalIntake

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -13,6 +13,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.commands.GyroCalibrateCommand;
 import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.IntakeToShooterSubsystem;
 import frc.robot.vision.Limelight;
 import frc.robot.webdashboard.DashboardLayout;
 import frc.robot.webdashboard.WebdashboardServer;
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,14 +1,22 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.DriveDefaultCommand;
+import frc.robot.commands.intakeToShootercommands.IntakeVerticalCommand;
 import frc.robot.control.CommandSwitchController;
 import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.IntakeToShooterSubsystem;
 
 public class RobotContainer {
 
+    public final Joystick payload = new Joystick(OperatorConstants.DRIVER_CONTROLLER_PORT);
+
     public final DriveSubsystem driveSubsystem;
+    public final IntakeToShooterSubsystem intakeToShooterSubsystem = new IntakeToShooterSubsystem();
+    public final IntakeVerticalCommand intakeVerticalCommand = new IntakeVerticalCommand(intakeToShooterSubsystem, () -> payload.getRawAxis(1));
     private final CommandSwitchController baseController = new CommandSwitchController(OperatorConstants.DRIVER_CONTROLLER_PORT);
     private final DriveDefaultCommand driveDefaultCommand;
 
@@ -20,7 +28,7 @@ public class RobotContainer {
     }
 
     private void configureBindings() {
-
+        CommandScheduler.getInstance().setDefaultCommand(intakeToShooterSubsystem, intakeVerticalCommand);
     }
 
     public Command getAutonomousCommand() {
